54dd1026b7c33a42f7be23a88c67ddadd5248819
dulatovazh <dulatovazh@gmail.com>
2025-06-18 22:32:50 +0300

    lesson3

---
+++ app.js
@@ -0,0 +1,45 @@
+const $slides = document.querySelectorAll(".slide");
+const $rightArrow = document.querySelector(".right-arrow");
+const $leftArrow = document.querySelector(".left-arrow");
+const $navs = document.querySelectorAll(".nav-item")
+
+const sliderProxy = new Proxy(
+    {
+        currentSlide: 0,
+    },
+    {
+        set: function (obj, prop, newval) {
+            obj[prop] = newval;
+            updateSlider(newval);
+        },
+    }
+);
+
+function updateSlider(currentSlide) {
+    const $slide = $slides[currentSlide];
+    const imageUrl = $slide.style.backgroundImage;
+    $slides.forEach((s) => s.classList.remove("active"));
+    $slide.classList.add("active");
+    setBackgroundImage(document.body, imageUrl);
+    const $activeNav = document.querySelector(".nav .nav-item.active");
+    $activeNav.classList.remove("active");
+    $navs[currentSlide].classList.add('active');
+}
+
+function setBackgroundImage(element, url) {
+    element.style.backgroundImage = url;
+}
+
+$rightArrow.onclick = () => {
+    if (sliderProxy.currentSlide + 1 >= $slides.length)
+        sliderProxy.currentSlide = 0;
+    else sliderProxy.currentSlide++;
+};
+
+$leftArrow.onclick = () => {
+    if (sliderProxy.currentSlide - 1 < 0)
+        sliderProxy.currentSlide = $slides.length - 1;
+    else sliderProxy.currentSlide--;
+};
+
+updateSlider(sliderProxy.currentSlide);
